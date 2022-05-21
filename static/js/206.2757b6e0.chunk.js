be1fe93b8805b88d77fc12c1a2896ac2bb82c395
DenisMalv <kissed03@gmail.com>
2022-05-21 13:53:37 +0300

    Updates

--- static/js/206.2757b6e0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework=self.webpackChunkreact_homework||[]).push([[206],{1206:function(e,r,n){n.r(r);var c=n(184);r.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("p",{children:"HomePAge"})})}}}]);
-//# sourceMappingURL=206.2757b6e0.chunk.js.map
---
+++ static/js/206.50d0d944.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework=self.webpackChunkreact_homework||[]).push([[206],{1206:function(e,r,n){n.r(r);var c=n(3329);r.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("p",{children:"HomePAge"})})}}}]);
+//# sourceMappingURL=206.50d0d944.chunk.js.map
